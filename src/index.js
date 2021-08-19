5e19f92c64e7d306c028e279bf31a5ed3d36032d
NikitaKakurin <neyasut87@rambler.ru>
2021-08-19 17:04:44 +0300

    init: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (!Array.isArray(array)){
+    return 0;
+  }else {
+  return array.filter(elem => typeof elem === 'number').length === 0 ? 0 : Math.min(...array);
+  };
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (!Array.isArray(array)){
+    return 0;
+  }else {
+  return array.filter(elem => typeof elem === 'number').length === 0 ? 0 : Math.max(...array);
+  };
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!Array.isArray(array)){
+    return 0;
+  }else {
+    return (array.filter(elem => typeof elem === 'number').length === 0) ? 0 : array.reduce((a, b) => a + b)/array.length;
+  };
+};
